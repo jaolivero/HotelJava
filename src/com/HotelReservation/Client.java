cd433d07a80d8d2c5d57fa6dd112cabf8da7b2b3
Joel Olivero <joelolivero@outlook.com>
2020-10-03 22:13:31 -0400

    made fields private for client

--- src/com/HotelReservation/Client.java
+++ src/com/HotelReservation/Client.java
@@ -1,13 +1,13 @@
 package com.HotelReservation;
 
 public class Client {
-    String name;
-    int partySize = 1;
-    float currentBill = 0;
-    float prepaid = 0;
-    String phoneNumber = "";
-    int roomNumber;
-    int roomType;
+    private String name;
+    private int partySize = 1;
+    private float currentBill = 0;
+    private float prepaid = 0;
+    private String phoneNumber = "";
+    private int roomNumber;
+    priavte int roomType;
     private Room room;
 
     public void makePayment() {
